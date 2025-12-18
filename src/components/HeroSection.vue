191eb99067478725b2c82d3b78a87b1fec487692
Mcky0101 <camaramacky73@gmail.com>
2025-12-18 16:28:42 +0000

    feat: Add Hero Section component with an image carousel and associated assets.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -7,15 +7,16 @@ const totalSlides = 8;
 let slideInterval: ReturnType<typeof setTimeout>;
 
 const images = [
-  { id: 1, src: '../../src/assets/image1.jpg', alt: 'Image 1' },
-  { id: 2, src: '../../src/assets/image2.jpg', alt: 'Image 2' },
-  { id: 3, src: '../../src/assets/image3.jpg', alt: 'Image 3' },
-  { id: 4, src: '../../src/assets/image4.jpg', alt: 'Image 4' },
-  { id: 5, src: '../../src/assets/image5.jpg', alt: 'Image 5' },
-  { id: 6, src: '../../src/assets/image6.jpg', alt: 'Image 6' },
-  { id: 7, src: '../../src/assets/image7.jpg', alt: 'Image 7' },
-  { id: 8, src: '../../src/assets/image8.jpg', alt: 'Image 8' }
+  { id: 1, src: '/images/image1.jpg', alt: 'Image 1' },
+  { id: 2, src: '/images/image2.jpg', alt: 'Image 2' },
+  { id: 3, src: '/images/image3.jpg', alt: 'Image 3' },
+  { id: 4, src: '/images/image4.jpg', alt: 'Image 4' },
+  { id: 5, src: '/images/image5.jpg', alt: 'Image 5' },
+  { id: 6, src: '/images/image6.jpg', alt: 'Image 6' },
+  { id: 7, src: '/images/image7.jpg', alt: 'Image 7' },
+  { id: 8, src: '/images/image8.jpg', alt: 'Image 8' }
 ];
+
 
 const contactInfo = ref({
   email: 'contact@abc2-group.com',
